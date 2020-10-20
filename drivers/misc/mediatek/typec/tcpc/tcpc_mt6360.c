5854c33da8435c886c1b29be7b819c6643e8b905
chihhao.chen <chihhao.chen@mediatek.com>
2020-10-20 16:09:24 +0800

    [ALPS05091732] TCPC: fix 10610506 coverity
    [Detail]
    fix 10610506 cert-c
    
    MTK-Commit-Id: fb6c70b719d31d209044e08b2033c015abf4756d
    
    Change-Id: I39b399c710b5d0e8981957114eb428fcb669db2d
    Signed-off-by: chihhao.chen <chihhao.chen@mediatek.com>
    CR-Id: ALPS05091732
    Feature: USB TYPE-C

--- drivers/misc/mediatek/typec/tcpc/tcpc_mt6360.c
+++ drivers/misc/mediatek/typec/tcpc/tcpc_mt6360.c
@@ -2369,6 +2369,7 @@ static int mt6360_tcpcdev_init(struct mt6360_chip *chip, struct device *dev)
 	struct device_node *np = dev->of_node;
 	u32 val, len;
 	const char *name = "default";
+	int err;
 
 	desc = devm_kzalloc(dev, sizeof(*desc), GFP_KERNEL);
 	if (!desc)
@@ -2420,7 +2421,9 @@ static int mt6360_tcpcdev_init(struct mt6360_chip *chip, struct device *dev)
 	}
 #endif	/* CONFIG_TCPC_VCONN_SUPPLY_MODE */
 
-	of_property_read_string(np, "mt-tcpc,name", (char const **)&name);
+	err = of_property_read_string(np, "mt-tcpc,name", (char const **)&name);
+	if (err < 0)
+		dev_info(dev, "%s no tcpc name\n", __func__);
 	len = strlen(name);
 	desc->name = kzalloc(len + 1, GFP_KERNEL);
 	if (!desc->name)
