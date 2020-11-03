ee8662651fce08ed933629e0d6d173ce1a876279
shangming.huang <shang-ming.huang@mediatek.com>
2020-11-04 06:56:11 +0800

    [ALPS05219044] lpm: fix memory release order
    update memory release order
    
    MTK-Commit-Id: 3a7133f7d87ed4ab619a352c55cf3dc25ab3b0b3
    
    Change-Id: Ie293a44ccb92fcc690f66d87200af5dbcff329a6
    Signed-off-by: shangming.huang <shang-ming.huang@mediatek.com>
    CR-Id: ALPS05219044
    Feature: Suspend

--- drivers/misc/mediatek/lpm/platform/mtk_lpm_irqremain.c
+++ drivers/misc/mediatek/lpm/platform/mtk_lpm_irqremain.c
@@ -112,13 +112,13 @@ int mtk_lpm_irqremain_get(struct mtk_lpm_irqremain **irq)
 	tar = kcalloc(1, sizeof(**irq), GFP_KERNEL);
 
 	if (!tar)
-		goto mtk_lpm_irqremain_release;
+		goto mtk_lpm_tar_fail;
 
 	tar->irqs = kcalloc(count,
 				sizeof(*tar->irqs), GFP_KERNEL);
 
 	if (!tar->irqs)
-		goto mtk_lpm_irqremain_release;
+		goto mtk_lpm_irq_fail;
 
 	tar->wakeup_src_cat = kcalloc(count,
 		sizeof(*tar->wakeup_src_cat), GFP_KERNEL);
@@ -147,15 +147,18 @@ int mtk_lpm_irqremain_get(struct mtk_lpm_irqremain **irq)
 
 	return 0;
 
+mtk_lpm_irqremain_release:
+	if (tar)
+		kfree(tar->wakeup_src);
 mtk_lpm_wakeup_src_cat_fail:
 	if (tar)
 		kfree(tar->wakeup_src_cat);
-mtk_lpm_irqremain_release:
+mtk_lpm_irq_fail:
 	if (tar) {
 		kfree(tar->irqs);
-		kfree(tar->wakeup_src);
 		kfree(tar);
 	}
+mtk_lpm_tar_fail:
 	mtk_lpm_system_unlock(flag);
 
 	return -ENOMEM;
