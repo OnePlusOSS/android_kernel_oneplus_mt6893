8a5ca44fc268fcf40b1835f8462ba14d2fc94313
dennis.chan <dennis.chan@mediatek.com>
2020-11-18 10:15:37 +0800

    [ALPS05413266] disp: Fix Coverity for 10747573
    Fix Coverity for 10747573
    
    MTK-Commit-Id: 35daa392eb09a4575df0c11656516bad41583dfb
    
    Change-Id: If2da199d2596b5c1216de016c068d433c1a32130
    Signed-off-by: dennis.chan <dennis.chan@mediatek.com>
    CR-Id: ALPS05413266
    Feature: Display Driver (DSI Interface)

--- drivers/gpu/drm/mediatek/mtk_dsi.c
+++ drivers/gpu/drm/mediatek/mtk_dsi.c
@@ -4716,19 +4716,21 @@ static int mtk_dsi_io_cmd(struct mtk_ddp_comp *comp, struct cmdq_pkt *handle,
 
 	case DSI_GET_MODE_BY_MAX_VREFRESH:
 	{
-		struct drm_display_mode *max_mode;
+		struct drm_display_mode *max_mode, *next;
 		unsigned int vrefresh = 0;
 
 		if (dsi == NULL)
 			break;
 
 		mode = (struct drm_display_mode **)params;
-		list_for_each_entry(max_mode, &dsi->conn.modes, head) {
-			if (max_mode) {
-				if (max_mode->vrefresh > vrefresh) {
-					vrefresh = max_mode->vrefresh;
-					*mode = max_mode;
-				}
+		list_for_each_entry_safe(max_mode, next, &dsi->conn.modes, head) {
+
+			if (max_mode == NULL)
+				break;
+
+			if (max_mode->vrefresh > vrefresh) {
+				vrefresh = max_mode->vrefresh;
+				*mode = max_mode;
 			}
 		}
 	}
