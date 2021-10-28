97fced5cb393d8405d89b2e108adc0ca99f749a6
LeoSHWeng <ot_leosh.weng@mediatek.com>
2021-10-28 14:19:01 +0800

    [ALPS06094688] vcodec: coverity issue fixed
    [Detial]
    	ALPS06094688, ALPS06148947
    	Out-of-bounds read: 10933470
    
    	ALPS06148944, ALPS06148948
    	CERT-C Characters and Strings: 10933471
    
    MTK-Commit-Id: b7ba5e3593f69c94fd284ee04298569d1f982725
    
    Change-Id: I8a59a5dfd67473fbaa8bd8265c3db3675e244ae2
    Signed-off-by: LeoSHWeng <ot_leosh.weng@mediatek.com>
    CR-Id: ALPS06094688
    Feature: [Module]Video Driver

--- drivers/media/platform/mtk-vcodec/mtk_vcodec_util.c
+++ drivers/media/platform/mtk-vcodec/mtk_vcodec_util.c
@@ -295,9 +295,8 @@ void mtk_vcodec_set_log(struct mtk_vcodec_ctx *ctx, char *val)
 		} else if ((i < argcMex-1) && strcmp("-mtk_v4l2_dbg_level", argv[i]) == 0) {
 			if (kstrtol(argv[++i], 0, &temp_val) == 0)
 				mtk_v4l2_dbg_level = temp_val;
-		} else {
+		} else if (i < argcMex-1) {
 			memset(vcu_log, 0x00, LOG_INFO_SIZE);
-
 			ret = snprintf(vcu_log, LOG_INFO_SIZE, "%s %s", argv[i], argv[i+1]);
 			if (ret < 0)
 				pr_info("[MTK_V4L2] vcu_log snprintf error: %d", ret);
