59b9c109a2fc4d5a58440e503054d490cf5b6c8f
yunhu she <yunhu.she@mediatek.com>
2021-11-25 13:36:10 +0800

    [ALPS06206709] FHCTL: MT6873 IPI_TIMEOUT_MS modify(REVERT)
    FHCTL: MT6873 IPI_TIMEOUT_MS change from 10ms to 2S(workaround).
    no need more
    
    MTK-Commit-Id: d97d246f061891b19922e61ed103ce853e056207
    
    CR-Id: ALPS06206709
    Signed-off-by: Yunhu She <Yunhu.She@mediatek.com>
    Feature: CPU DVFS (CPU Dynamic Voltage and Frequency Scaling)
    Change-Id: Iafdd9a5487d4ddff20a0a655e0c328eafc111fb0

--- drivers/misc/mediatek/freqhopping/fhctl_new/clk-fhctl-mcupm.c
+++ drivers/misc/mediatek/freqhopping/fhctl_new/clk-fhctl-mcupm.c
@@ -17,11 +17,7 @@
 #include "mcupm_ipi_id.h"
 
 #define FHCTL_TARGET FHCTL_MCUPM
-#if defined(CONFIG_MACH_MT6873)
-	#define IPI_TIMEOUT_MS 2000
-#else
-	#define IPI_TIMEOUT_MS 10
-#endif
+#define IPI_TIMEOUT_MS 10
 
 struct match {
 	char *name;
