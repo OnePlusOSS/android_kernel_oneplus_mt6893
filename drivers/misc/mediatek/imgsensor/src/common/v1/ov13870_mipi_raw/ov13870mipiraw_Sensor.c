73932847f9f02a1e5bf6ad45101d67528e7dc001
RexYHH <ot_rex.hsueh@mediatek.com>
2021-12-05 11:26:56 +0800

    [ALPS06281168] Coverity: CERT-C Error Handling
    CID: 10449677
    
    MTK-Commit-Id: 85b2f38265ca4d21255566fa77a426d9db2848e2
    
    Signed-off-by: RexYHH <ot_rex.hsueh@mediatek.com>
    CR-Id: ALPS06281168
    Feature: Camcorder Application
    Change-Id: I8f9d5e6c19f64005d61b527deea1acfea809551a

--- drivers/misc/mediatek/imgsensor/src/common/v1/ov13870_mipi_raw/ov13870mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1/ov13870_mipi_raw/ov13870mipiraw_Sensor.c
@@ -1861,15 +1861,19 @@ static kal_uint32 feature_control(MSDK_SENSOR_FEATURE_ENUM feature_id, UINT8 *fe
 		break;
 	case SENSOR_FEATURE_GET_PDAF_TYPE:
 		*feature_para = pdaf_sensor_type;
-		if (pdaf_sensor_type == PDAF_NO_PDAF)
-			sprintf(feature_para, "configure as type 1");
-		else if (pdaf_sensor_type == PDAF_VC_TYPE)
-			sprintf(feature_para, "configure as type 2");
-		else if (pdaf_sensor_type == PDAF_RAW_TYPE)
-			sprintf(feature_para, "configure as type 3");
-		else
-			sprintf(feature_para, "configure as unknown type");
-
+		if (pdaf_sensor_type == PDAF_NO_PDAF) {
+			if (sprintf(feature_para, "configure as type 1") < 0)
+				LOG_INF("PDAF_NO_PDAF sprintf fail\n");
+		} else if (pdaf_sensor_type == PDAF_VC_TYPE) {
+			if (sprintf(feature_para, "configure as type 2") < 0)
+				LOG_INF("PDAF_VC_TYPE sprintf fail\n");
+		} else if (pdaf_sensor_type == PDAF_RAW_TYPE) {
+			if (sprintf(feature_para, "configure as type 3") < 0)
+				LOG_INF("PDAF_RAW_TYPE sprintf fail\n");
+		} else {
+			if (sprintf(feature_para, "configure as unknown type") < 0)
+				LOG_INF("configure as unknown type sprintf fail\n");
+		}
 		LOG_INF("get PDAF type = %d\n", pdaf_sensor_type);
 		break;
 	case SENSOR_FEATURE_SET_PDAF_TYPE:
